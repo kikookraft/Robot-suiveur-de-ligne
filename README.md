b05b3ee66aa5f204a7649d4077cec13e1f125102
kikookraft <kikookraft.teamcraft@gmail.com>
2024-02-08 19:18:15 +0100

    Nettoyage code + ajouts pins des deux moteurs

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 
+## Info importante
+Le projet à été créé avec PlatformIO avec VSCode.
+Pour plus d'info sur PlatformIO : [PlatformIO](https://platformio.org/)  
+  
+Sinon pour juste voir le code, tout se situe dans le dossier `src` et le fichier `main.cpp` est le point d'entrée du programme.
+
 Liens utiles : 
 - [Utilisation module infrarouge](https://www.instructables.com/How-to-Use-TCRT5000-IR-Sensor-Module-With-Arduino-/)  
    Doit être a moin de 4.5cm du sol sinon il ne détecte pas correctement le sol.
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,40 @@
 #include <Arduino.h>
 
-// declaration des fonctions
-void avance();
-void arriere();
-void stop();
-void vitesse(int vitesse);
-void test1();
-void test2();
-void test3();
+// avancer moteur 1
+void forwardMotor1();
+// reculer moteur 1
+void backwardMotor1();
+// arreter moteur 1
+void stopMotor1();
+// regler la vitesse du moteur 1 (0-255)
+void speedMotor1(int vitesse);
+// avancer moteur 2
+void forwardMotor2();
+// reculer moteur 2
+void backwardMotor2();
+// arreter moteur 2
+void stopMotor2();
+// regler la vitesse du moteur 2 (0-255)
+void speedMotor2(int vitesse);
 
-//Ports de commande du moteur B
+// fonction test
+void test1();
+
+//Ports de commande des moteurs
 #define motorIN1 7
 #define motorIN2 6
 #define motorIN3 5
 #define motorIN4 4
 #define ENB 10
 #define ENA 11
-#define MIN_PWM 50
 
-#define capteur 13
- 
-// gestions du moteur
+#define MIN_PWM 50 // vitesse minimale du moteur pour eviter de caler
+
+// capteurs
+#define CAPTEUR_LEFT 13
+#define CAPTEUR_RIGHT 12
+
+// gestions du moteur (temporaire)
 int state = MIN_PWM; // evite de demarrer a 0 car le moteur ne demarre pas
 int down = 0;
 int dir = 1; // 1 = avance, 0 = recule
@@ -29,89 +43,70 @@ int test = 0;
 
 void setup() {
     // Configuration des ports en mode "sortie"
-    pinMode(motorIN3, OUTPUT);
-    pinMode(motorIN4, OUTPUT);
-    pinMode(ENB, OUTPUT);
-
-    pinMode(capteur, INPUT);
-    avance();
+  pinMode(motorIN1, OUTPUT);
+  pinMode(motorIN2, OUTPUT);
+  pinMode(motorIN3, OUTPUT);
+  pinMode(motorIN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(CAPTEUR_LEFT, INPUT);
+  pinMode(CAPTEUR_RIGHT, INPUT);
+  // Initialisation des moteurs
+  stopMotor1();
+  stopMotor2();
+  speedMotor1(0);
+  speedMotor2(0);
 }
- 
+
+// ----------------- Boucle principale -----------------
 void loop() {
-  test3();
-  //test2();
-  //test1();
+  test1();
 }
 
-void test3() {
-  if (digitalRead(capteur) == LOW) {
-    stop();
-  } else {
-    avance();
-    vitesse(255);
-  }
-  delay(5);
 
-}
 
-void test2() {
-   avance();
-  vitesse(255);
-  delay(2000);
-  stop();
-  delay(1000);
-  arriere();
-  vitesse(255);
-  delay(2000);
-  stop();
-  delay(1000);
-}
-
-void test1() {
-  // gestion de la vitesse
-  if (state < 255 && down == 0) {
-    state++;
-  } else {
-    down = 1;
-  }
-  if (state > MIN_PWM && down == 1) {
-    state--;
-  } else {
-    down = 0;
-  }
-
-  // gestion de la direction
-  if (state == MIN_PWM && down == 1) {
-    if (dir == 1) {
-      arriere();
-      dir = 0;
-    } else {
-      avance();
-      dir = 1;
-    }
-    state = MIN_PWM;
-  }
-
-  vitesse(state);
-  delay(5);
-
-}
-
-void avance() {
+// ----------------- Moteur 1 -----------------
+void forwardMotor1() {
   digitalWrite(motorIN3, HIGH);
   digitalWrite(motorIN4, LOW);
 }
 
-void arriere() {
+void backwardMotor1() {
   digitalWrite(motorIN3, LOW);
   digitalWrite(motorIN4, HIGH);
 }
 
-void stop() {
+void stopMotor1() {
   digitalWrite(motorIN3, LOW);
   digitalWrite(motorIN4, LOW);
 }
 
-void vitesse(int vitesse) {
+void speedMotor1(int vitesse) {
   analogWrite(ENB, vitesse);
+}
+
+// ----------------- Moteur 2 -----------------
+void forwardMotor2() {
+  digitalWrite(motorIN1, HIGH);
+  digitalWrite(motorIN2, LOW);
+}
+
+void backwardMotor2() {
+  digitalWrite(motorIN1, LOW);
+  digitalWrite(motorIN2, HIGH);
+}
+
+void stopMotor2() {
+  digitalWrite(motorIN1, LOW);
+  digitalWrite(motorIN2, LOW);
+}
+
+void speedMotor2(int vitesse) {
+  analogWrite(ENA, vitesse);
+}
+
+
+// ----------------- Fonctions de tests random -----------------
+void test1() {
+  
 }
