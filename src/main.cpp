97ba795da1d8a79a1ac67e22b66a3d72c6022702
kikookraft <kikookraft.teamcraft@gmail.com>
2024-02-09 02:06:33 +0100

    Déplacement des fonctions du moteur dans une autre fonction

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,23 @@
 #include <Arduino.h>
+// import des fonctions utiles pour le moteur
+# include "motor.h"
 
-// avancer moteur 1
-void forwardMotor1();
-// reculer moteur 1
-void backwardMotor1();
-// arreter moteur 1
-void stopMotor1();
-// regler la vitesse du moteur 1 (0-255)
-void speedMotor1(int vitesse);
-// avancer moteur 2
-void forwardMotor2();
-// reculer moteur 2
-void backwardMotor2();
-// arreter moteur 2
-void stopMotor2();
-// regler la vitesse du moteur 2 (0-255)
-void speedMotor2(int vitesse);
-
-// smooth le démarrage / arret des moteurs
-void smoothStartMotor1();
-void smoothStopMotor1();
-void smoothStartMotor2();
-void smoothStopMotor2();
-
-
-
-// fonction test
+// déclaration des fonctions de test
 void test1();
 void test2();
-
-//Ports de commande des moteurs
-#define motorIN1 7
-#define motorIN2 6
-#define motorIN3 5
-#define motorIN4 4
-#define ENB 10
-#define ENA 11
-
-#define MIN_PWM 50 // vitesse minimale du moteur pour eviter de caler
 
 // capteurs
 #define CAPTEUR_LEFT 13
 #define CAPTEUR_RIGHT 12
 
-// lissage du démarrage / arret des moteurs
-#define SMOOTHING 1
-// gestions du moteur (temporaire)
-int PWM1 = 0; // vitesse moteur 1
-int PWM2 = 0; // vitesse moteur 2
-
+// ----------------- Initialisation -----------------
 void setup() {
-    // Configuration des ports en mode "sortie"
-  pinMode(motorIN1, OUTPUT);
-  pinMode(motorIN2, OUTPUT);
-  pinMode(motorIN3, OUTPUT);
-  pinMode(motorIN4, OUTPUT);
-  pinMode(ENB, OUTPUT);
-  pinMode(ENA, OUTPUT);
+  // Configuration des ports en mode "sortie"
   pinMode(CAPTEUR_LEFT, INPUT);
   pinMode(CAPTEUR_RIGHT, INPUT);
-  // Initialisation des moteurs
-  stopMotor1();
-  stopMotor2();
-  speedMotor1(0);
-  speedMotor2(0);
+  initMotor();
 
+  // Initialisation de la communication série (utile pour debug)
   Serial.begin(9600);
 }
 
@@ -75,138 +28,37 @@ void loop() {
 }
 
 
-
-// ----------------- Moteur 1 -----------------
-void forwardMotor1() {
-  digitalWrite(motorIN3, HIGH);
-  digitalWrite(motorIN4, LOW);
-}
-
-void backwardMotor1() {
-  digitalWrite(motorIN3, LOW);
-  digitalWrite(motorIN4, HIGH);
-}
-
-void stopMotor1() {
-  digitalWrite(motorIN3, LOW);
-  digitalWrite(motorIN4, LOW);
-}
-
-void speedMotor1(int vitesse) {
-  analogWrite(ENB, vitesse);
-}
-
-// ----------------- Moteur 2 -----------------
-void forwardMotor2() {
-  digitalWrite(motorIN1, HIGH);
-  digitalWrite(motorIN2, LOW);
-}
-
-void backwardMotor2() {
-  digitalWrite(motorIN1, LOW);
-  digitalWrite(motorIN2, HIGH);
-}
-
-void stopMotor2() {
-  digitalWrite(motorIN1, LOW);
-  digitalWrite(motorIN2, LOW);
-}
-
-void speedMotor2(int vitesse) {
-  analogWrite(ENA, vitesse);
-}
-
-// ----------------- smooth le démarrage / arret des moteurs -----------------
-// ces fonctions ne doivent pas etre bloquantes
-void smoothStartMotor1(int direction) {
-  if (PWM1 < MIN_PWM) {
-    PWM1 = MIN_PWM;
-  } else if (PWM1 + SMOOTHING < 255) {
-    PWM1 += SMOOTHING;
-  } else if (PWM1 + SMOOTHING > 255) {
-    PWM1 = 255;
-  }
-  
-  if (direction == 1) {
-    forwardMotor1();
-  } else {
-    backwardMotor1();
-  }
-  speedMotor1(PWM1);
-  delay(1);
-}
-
-void smoothStopMotor1() {
-  if (PWM1 > MIN_PWM) {
-    PWM1 -= SMOOTHING;
-  } else {
-    PWM1 = 0;
-    stopMotor1();
-  }
-  speedMotor1(PWM1);
-  delay(1);
-}
-
-//moteur 2
-void smoothStartMotor2(int direction) {
-  if (PWM2 < MIN_PWM) {
-    PWM2 = MIN_PWM;
-  } else if (PWM2 + SMOOTHING < 255) {
-    PWM2 += SMOOTHING;
-  } else if (PWM2 + SMOOTHING > 255) {
-    PWM2 = 255;
-  }
-
-  if (direction == 1) {
-    forwardMotor2();
-  } else {
-    backwardMotor2();
-  }
-  speedMotor2(PWM2);
-  delay(1);
-}
-
-void smoothStopMotor2() {
-  if (PWM2 > MIN_PWM) {
-    PWM2 -= SMOOTHING;
-  } else {
-    PWM2 = 0;
-    stopMotor2();
-  }
-  speedMotor2(PWM2);
-  delay(1);
-}
-
 // ----------------- Fonctions de tests random -----------------
 void test1() {
+  // ce test allume les moteurs de manière "fluide", c'est à dire que la vitesse augmente progressivement et décroit progressivement
+  // et ceci en fonction du capteur activé
+
   // si le capteur gauche est actif allumer moteur 1
   if (digitalRead(CAPTEUR_LEFT) == LOW) {
     smoothStartMotor1(1);
-    // Serial.println(PWM1);
   } else {
     smoothStopMotor1();
   }
   // si le capteur droit est actif allumer moteur 2
   if (digitalRead(CAPTEUR_RIGHT) == LOW) {
     smoothStartMotor2(1);
-    // Serial.println(PWM2);
   } else {
     smoothStopMotor2();
   }
 }
 
 void test2() {
-  // comme test 1 mais sans la fluidité
+  // ce test allume les moteurs de manière "brutale", c'est à dire que la vitesse augmente directement à la vitesse maximale
+  // et ceci en fonction du capteur activé
+
   if (digitalRead(CAPTEUR_LEFT) == LOW) {
     forwardMotor1();
-    Serial.println("capteur gauche");
     speedMotor1(255);
   } else {
     stopMotor1();
   }
   if (digitalRead(CAPTEUR_RIGHT) == LOW) {
     forwardMotor2();
-    Serial.println("capteur droit");
     speedMotor2(255);
   } else {
     stopMotor2();
---
+++ src/motor.h
@@ -0,0 +1,162 @@
+#include <Arduino.h>
+
+//Ports de commande du pont en H (pour les deux moteurs)
+#define motorIN1 7
+#define motorIN2 6
+#define motorIN3 5
+#define motorIN4 4
+#define ENB 10
+#define ENA 11
+
+// vitesse minimale du moteur pour eviter de caler (utilisé pour les fonction smooth...)
+#define MIN_PWM 50
+
+// lissage du démarrage / arret des moteurs
+#define SMOOTHING 2 // valeur à ajouter ou retirer à la vitesse
+#define SMOOTHING_DELAY 1 // temps d'attente entre chaque ajout ou retrait de vitesse
+
+// TODO: intégrer ces variables dans une structure ou dans des classes pour éviter les variables globales
+int PWM1 = 0; // vitesse moteur 1
+int PWM2 = 0; // vitesse moteur 2
+
+// ----------------- Initialisation des moteurs -----------------
+void initMotor() {
+  // tout est en sortie
+  pinMode(ENA, OUTPUT); // moteur 1
+  pinMode(motorIN1, OUTPUT); // moteur 1
+  pinMode(motorIN2, OUTPUT); // moteur 1
+
+  pinMode(ENB, OUTPUT); // moteur 2
+  pinMode(motorIN3, OUTPUT); // moteur 2
+  pinMode(motorIN4, OUTPUT); // moteur 2
+}
+
+
+// ----------------- Moteur 1 -----------------
+// avancer moteur 1
+void forwardMotor1() {
+  digitalWrite(motorIN1, HIGH);
+  digitalWrite(motorIN2, LOW);
+}
+
+// reculer moteur 1
+void backwardMotor1() {
+  digitalWrite(motorIN1, LOW);
+  digitalWrite(motorIN2, HIGH);
+}
+
+// arreter moteur 1
+void stopMotor1() {
+  digitalWrite(motorIN1, LOW);
+  digitalWrite(motorIN2, LOW);
+}
+
+// vitesse moteur 1
+void speedMotor1(int vitesse) {
+  analogWrite(ENA, vitesse);
+}
+
+
+// ----------------- Moteur 2 -----------------
+// avancer moteur 2
+void forwardMotor2() {
+  digitalWrite(motorIN3, HIGH);
+  digitalWrite(motorIN4, LOW);
+}
+
+// reculer moteur 2
+void backwardMotor2() {
+  digitalWrite(motorIN3, LOW);
+  digitalWrite(motorIN4, HIGH);
+}
+
+// arreter moteur 2
+void stopMotor2() {
+  digitalWrite(motorIN3, LOW);
+  digitalWrite(motorIN4, LOW);
+}
+
+// vitesse moteur 2
+void speedMotor2(int vitesse) {
+  analogWrite(ENB, vitesse);
+}
+
+
+// ----------------- smooth le démarrage / arret des moteurs -----------------
+// fonction qui necessite d'être appelée plusieures fois (explication de ce choix dans la doc partie #moteurs)
+// démarage fluide moteur 1
+void smoothStartMotor1(int direction) {
+  if (PWM1 < MIN_PWM) {
+    PWM1 = MIN_PWM;
+  } else if (PWM1 + SMOOTHING < 255) {
+    PWM1 += SMOOTHING;
+  } else if (PWM1 + SMOOTHING > 255) {
+    PWM1 = 255;
+  }
+  
+  // si direction = 1 alors avancer, sinon reculer
+  // TODO: faire en sorte que cette partie ne soit pas éxécutée à chaque appel de la fonction
+  if (direction == 1) {
+    forwardMotor1();
+  } else {
+    backwardMotor1();
+  }
+
+  // appliquer la vitesse
+  speedMotor1(PWM1);
+  delay(SMOOTHING_DELAY);
+}
+
+// arret fluide moteur 1
+void smoothStopMotor1() {
+  if (PWM1 > MIN_PWM) {
+    PWM1 -= SMOOTHING;
+  } else {
+    // si la vitesse est inférieure à la vitesse minimale alors on arrete le moteur directement
+    PWM1 = 0;
+    stopMotor1();
+  }
+
+  // appliquer la vitesse
+  speedMotor1(PWM1);
+  delay(SMOOTHING_DELAY);
+}
+
+// --- la même chose pour le moteur 2 ---
+// démarage fluide moteur 2
+void smoothStartMotor2(int direction) {
+  if (PWM2 < MIN_PWM) {
+    PWM2 = MIN_PWM;
+  } else if (PWM2 + SMOOTHING < 255) {
+    PWM2 += SMOOTHING;
+  } else if (PWM2 + SMOOTHING > 255) {
+    PWM2 = 255;
+  }
+
+  // si direction = 1 alors avancer, sinon reculer
+  // TODO: faire en sorte que cette partie ne soit pas éxécutée à chaque appel de la fonction
+  if (direction == 1) {
+    forwardMotor2();
+  } else {
+    backwardMotor2();
+  }
+
+  // appliquer la vitesse
+  speedMotor2(PWM2);
+  delay(SMOOTHING_DELAY);
+}
+
+// arret fluide moteur 2
+void smoothStopMotor2() {
+  if (PWM2 > MIN_PWM) {
+    PWM2 -= SMOOTHING;
+  } else {
+    // si la vitesse est inférieure à la vitesse minimale alors on arrete le moteur directement
+    PWM2 = 0;
+    stopMotor2();
+  }
+
+  // appliquer la vitesse
+  speedMotor2(PWM2);
+  delay(SMOOTHING_DELAY);
+}
